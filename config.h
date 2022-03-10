e6ef11fbe4ea98c19ea009ba4bd8ffc6e4b400de
Diamond Bond <diamond.nigel75@Gmail.com>
2022-03-11 00:29:15 +0530

    update
    Signed-off-by: Diamond Bond <diamond.nigel75@Gmail.com>

--- config.h
+++ config.h
@@ -10,8 +10,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;     /* 0 means no bar */
 static const int topbar             = 1;     /* 0 means bottom bar */
-/* static const char *fonts[]          = { "terminus:size=10" }; */
-/* static const char dmenufont[]       = "terminus:size=10"; */
+/* static const char *fonts[]          = { "terminus:size=12" }; */
+/* static const char dmenufont[]       = "terminus:size=12"; */
 /* static const char col_gray1[]       = "#000000"; */
 /* static const char col_gray2[]       = "#AAAAAA"; */
 /* static const char col_gray3[]       = "#ffffff"; */
@@ -26,8 +26,8 @@ static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_gray3, col_gray1, col_cyan },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_gray2 },
 };
 
 /* tagging */
