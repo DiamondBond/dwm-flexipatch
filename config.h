0c6e40ea23f6d54c797b8047bdbeb8e2ff55ce3b
Diamond Bond <diamond.nigel75@Gmail.com>
2022-03-18 05:33:17 +0530

    fix dwmstatus
    Signed-off-by: Diamond Bond <diamond.nigel75@Gmail.com>

--- config.h
+++ config.h
@@ -83,7 +83,7 @@ static const char *const autostart[] = {
 	"sh", "-c", "/home/diamond/bin/disable_touchscreen.sh", NULL,
 	"hsetroot", "-solid", "darkgray", NULL,
 	"xrdb", "/home/diamond/.Xresources", NULL,
-	"sh", "-c", "while :; do dwmstatus.sh -; sleep 60; done", NULL,
+	"sh", "-c", "while :; do /home/diamond/bin/dwmstatus.sh -; sleep 60; done", NULL,
 	"sh", "-c", "/home/diamond/bin/startcompositor", NULL,
 	"dunst", NULL,
 	/* "xss-lock", "slock", NULL, */
